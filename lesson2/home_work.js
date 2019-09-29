9df9d9f9217550c253cbe020a1239d0c2f66c368
unknown <mariana.seryvko@gmail.com>
2019-09-30 00:23:56 +0300

    Lesson two homework

---
+++ lesson2/home_work.js
@@ -0,0 +1,126 @@
+// 1
+
+// const login = prompt('Please log in');
+// if (login === null ) {
+// 	console.log('Bye');
+// } else if (login === 'Admin') {
+// 	console.log('Please enter password');
+// 	const password = prompt('Please enter password');
+// } else if (login != 'Admin') {
+// 	console.log('I dont know you');
+// } else if (password ==='password') {
+// 	console.log('Hello Admin');
+// } else if (password ==='password') {
+// 	console.log('Wrong password');
+// }
+
+
+// 2
+
+// const a = 100500;
+// const b = 1993;
+//  if (a > b) {
+//  	console.log(a);
+//  } else if (a < b) {
+// 	console.log(b);
+// }
+
+ // 3
+
+// const a = 1993;
+// if (a%2===1) {
+// 	console.log('Odd');
+// } else if (a%2===0) {
+// 	console.log('Even');
+// }
+
+ // 4
+
+ // const number = prompt('Please enter number');
+ // if (number < 5) {
+ // 	console.log('Tiny')
+ // } else if (number < 10) {
+ // 	console.log('Small');
+ // } else if (number < 15) {
+ // 	console.log('Medium');
+ // } else if (number < 20) {
+ // 	console.log('Large');
+ // } else if (number >= 20) {
+ // 	console.log('Huge');
+ // }
+
+ // 5
+
+ // const mark = prompt('Please enter your mark');
+ // if (mark === 'A') {
+ // 	console.log('Ваш бал більше 90');
+ // } else if (mark === 'B') {
+ // 	console.log('Ваш бал більше 80');
+ // } else if (mark === 'C') {
+ // 	console.log('Ваш бал більше 70');
+ // } else if (mark === 'D') {
+ // 	console.log('Ваш бал більше 65');
+ // } else {
+ // 	console.log('Незадовільно');
+ // }
+ 
+ // 6
+
+ // const name = prompt('Please enter your name');
+ // if (name === 'Ostap' ||name === 'Mykhailo' ||name === 'Nazar') {
+ // 	console.log('Mens name');
+ // } else if (name === 'Mariana' ||name ==='Anastasia' || name ==='Oksana'){ 
+ // 	console.log('Womens name');
+ // } else {
+ // 	console.log('I dont know such name');
+ // }
+
+ // 7
+
+ // const number = prompt('Please enter your number');
+ // if (number === 0) {
+ // 	console.log('You entered zero');
+ // } else if (number != 1&& number !=3&& number !=5&& number !=7&& number !=9) {
+ // 	console.log('even');
+ // } else if (number != 2&& number !=4&& number !=6&& number !=8) {
+ // 	console.log('odd'); 
+ // }
+
+
+ // 8
+
+ // var name = prompt('Please enter your name');
+ // var age  = 123
+ // if (name === 'Admin' || name === 'admin') {
+ // 	age = prompt('Please enter your age');
+ // } else if (name != 'Admin' || name != 'admin') {
+ // 	console.log('I dont know you');
+ // } if (age > 20) {
+ // 	var password = prompt('Please enter password');
+ // } if (password === 'Password' || password === 'password') {
+ // 	console.log('Welcome');
+ // } else if (age < 20) {
+ // 	console.log('You are too young');
+ // } 
+
+ // 9
+
+
+const result = prompt('Please put value');
+const a = 10;
+const c = 2;
+	console.log(a);
+if (result === '*') {
+	console.log(typeof a);
+	const mult=a*c;
+	console.log(mult);
+} else if (result === '-') {
+	console.log(a-c);
+} 
+
+// 10
+
+
+
+
+
